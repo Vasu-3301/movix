2abea129cc59aa4c119d126dd5ee7052068ef7bb
vasudev <vasudev3301@gmail.com>
2023-03-07 12:57:02 +0530

    movix app code completed

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import useFetch from "../../../hooks/useFetch";
+import Img from "../../../components/lazyLoadImage/img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import "./style.scss";
+
+const HeroBanner = () => {
+  const [background, SetBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    // ? i.e optional chaning is used bacause data is getting from server
+    // Math.floor and Math.random is used to generate random value from 20
+    // .backdrop_path is in the API
+
+    const bg =
+      url.backdrop +
+      data?.results[Math?.floor(Math?.random() * 20)].backdrop_path;
+    SetBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
+      <div className="opacity-layer"></div>
+
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Millions of Movies, TV Shows and people to discover,Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for Movies or TV Shows...."
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
